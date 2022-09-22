073485a275ba4e62d0d0b27930b5fd65fc5fac0b
Spencer Long <spencerlong0624@gmail.com>
2022-09-22 15:28:04 -0400

    team name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("name")) {
-            return "MyTeam";
+            return "cache-carti";
         }
         if (query.toLowerCase().contains("spencer")) {
             return "Good Morning";
